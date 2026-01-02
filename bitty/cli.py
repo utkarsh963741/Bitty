69e444486ae2bf2f8de0efa6e55dfd163455b2f0
Utkarsh Singh <utkarsh.singh27800@gmail.com>
2026-01-02 17:18:39 +0530

    implement Torrent wrapper class to expose properties of .torrent file

--- bitty/cli.py
+++ bitty/cli.py
@@ -1,2 +1,14 @@
+
+from bitty.bencoding import Decoder
+from bitty.torrent import Torrent
+
 def main():
     print("Bitty CLI is running")
+    with open('tests/data/ubuntu-16.04-desktop-amd64.iso.torrent', 'rb') as f:
+        meta_info = f.read()
+        torrent = Decoder(meta_info).decode()
+        for key, value in list(torrent.items())[:-1]:
+            print(key, value)
+    
+    file = Torrent('tests/data/ubuntu-16.04-desktop-amd64.iso.torrent')
+    print("Announce URL:", file.announce)
